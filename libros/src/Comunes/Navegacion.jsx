de8a26bcfbc03f9c498f6056d5859c11f4eeb5e4
Stry12 <142692315+Stry12@users.noreply.github.com>
2023-09-22 20:27:21 -0300

    Delete libros directory

--- libros/src/Comunes/Navegacion.jsx
+++
@@ -1,9 +0,0 @@
-import SimpleBottomNavigation from "./SimpleBottomNavigation";
-import "C:/Users/Salvador/OneDrive - alumnos.uv.cl/Escritorio/sexo/kellun_proyect/Proyecto-ICI324/libros/src/App.css"
-export default function Navegacion() {
-
-  return (<>
-    <SimpleBottomNavigation></SimpleBottomNavigation>
-  </>)
-}
-
