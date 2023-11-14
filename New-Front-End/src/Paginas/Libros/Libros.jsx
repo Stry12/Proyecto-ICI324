6851a2d0b8f6f7d7abd858ff5cd792a6d084561e
Stry12 <salvador.gallardo@alumnos.uv.cl>
2023-11-14 16:11:33 -0300

    Buscador de libros

--- New-Front-End/src/Paginas/Libros/Libros.jsx
+++ New-Front-End/src/Paginas/Libros/Libros.jsx
@@ -25,6 +25,17 @@ const Libros = () => {
       .catch((error) => console.error('Error al obtener datos de la API:', error));
   }, []);
 
+  console.log(    fetch(`${apiUrl}/libros/getlibros`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Error en la solicitud');
+    }
+    return response.json();
+  })
+  .then((jsonData) => setData(jsonData.libros))
+  .catch((error) => console.error('Error al obtener datos de la API:', error))
+);
+
   return (
     <>
       <NavBar />
--- New-Front-End/src/Paginas/Tradeos/SubPaginas/AgregarPublicacion/AgregarPublicacion.jsx
+++ New-Front-End/src/Paginas/Tradeos/SubPaginas/AgregarPublicacion/AgregarPublicacion.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   TextField,
   Button,
@@ -7,15 +7,65 @@ import {
   Select,
   MenuItem,
   Paper,
-  Grid
+  Grid,
+  Autocomplete
 } from '@mui/material';
 import NavBar from '../../../../Componentes/Navbar.jsx';
-import '../../../../App.css'
-
+import '../../../../App.css';
 
 const apiUrl = 'http://localhost:4000';
 
 const AgregarPublicacion = () => {
+  // Buscador
+  const buscados = [];
+
+  const [top100Films, setBooks] = useState([]);
+
+
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/libros/librosisbn`);
+        const data = await response.json();
+  
+        if (data && data.libros) {
+          const formattedBooks = data.libros.map((libro) => ({
+            title: libro.Titulo,
+            isbn: libro.ISBN,
+          }));
+  
+          setBooks(formattedBooks);
+  
+          // Imprimir en la consola
+          console.log('Lista de libros:', formattedBooks);
+        } else {
+          console.log('La respuesta de la API no contiene datos esperados:', data);
+        }
+      } catch (error) {
+        console.error('Error al obtener los libros:', error);
+      }
+    };
+  
+    fetchBooks();
+  }, []);
+    
+
+
+  const options = top100Films.map((option) => {
+    const firstLetter = option.title[0].toUpperCase();
+    return {
+      firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+      ...option,
+    };
+  });
+
+
+
+  //Fin Buscador
+
+
+
   const [title, setTitle] = useState('');
   const [isbn, setIsbn] = useState('');
   const [condition, setCondition] = useState('');
@@ -32,79 +82,102 @@ const AgregarPublicacion = () => {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    // Aquí puedes agregar la lógica para enviar la información al servidor
   };
 
   return (
     <>
-    <NavBar></NavBar>
-    <Paper
-      elevation={3}
-      sx={{
-        padding: '20px',
-        margin: '20px',
-        backgroundColor: '#f0f0f0',
-        border: '1px solid #ccc',
-        borderRadius: '8px',
-        boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)',
-      }}
-    >
-      <form onSubmit={handleFormSubmit}>
-        <TextField
-          label="Título del Libro"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-          fullWidth
-        />
-        <TextField
-          label="ISBN"
-          value={isbn}
-          onChange={(e) => setIsbn(e.target.value)}
-          required
-          fullWidth
-        />
-        <FormControl fullWidth>
-          <InputLabel>Estado del Libro</InputLabel>
-          <Select
-            value={condition}
-            onChange={handleConditionChange}
-            required
-          >
-            <MenuItem value="Nuevo">Nuevo</MenuItem>
-            <MenuItem value="Casi Nuevo">Casi Nuevo</MenuItem>
-            <MenuItem value="Usado">Usado</MenuItem>
-          </Select>
-        </FormControl>
-        <input
-          type="file"
-          accept="image/*"
-          multiple
-          onChange={handleImageUpload}
-        />
-        {images && images.length > 0 && (
-          <Grid container spacing={2} 
-          sx={{ marginTop: '10px',
-                alignItems: 'center',
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                
-          }}>
-            {Array.from(images).map((image, index) => (
-                <Grid item key={index} xs={12} sm={6} md={4} lg={3} xl={2}>
-                  <img
-                    className='pre-imagenes-img'
-                    key={index}
-                    src={URL.createObjectURL(image)}
-                  />
+      <NavBar></NavBar>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: '20px',
+          margin: '20px',
+          backgroundColor: '#f0f0f0',
+          border: '1px solid #ccc',
+          borderRadius: '8px',
+          boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)',
+        }}
+      >
+        <form onSubmit={handleFormSubmit}>
+          <Grid container spacing={2}>
+            {/* Columna izquierda para cargar imágenes */}
+            <Grid item xs={12} sm={8} md={8} lg={6} xl={6} className='container-imagen'>
+              <input
+                type="file"
+                accept="image/*"
+                multiple
+                onChange={handleImageUpload}
+              />
+              {images && images.length > 0 && (
+                <Grid container spacing={2} className='container-imagen-publi'>
+                  {Array.from(images).map((image, index) => (
+                      <Grid item key={index} xs={12} sm={10} md={8} lg={6} xl={3}>
+                        <img
+                          className='container-imagen-publi-img'
+                          key={index}
+                          src={URL.createObjectURL(image)}
+                        />
+                      </Grid>
+                  ))}
                 </Grid>
-            ))}
+              )}
+            </Grid>
+
+            {/* Columna derecha para campos de texto */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Título del Libro"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+                fullWidth
+                sx = {{ mb: 2}}
+              />
+
+              <TextField
+                label="ISBN"
+                value={isbn}
+                onChange={(e) => setIsbn(e.target.value)}
+                required
+                fullWidth
+                sx = {{ mb: 2}}
+              />
+              <FormControl fullWidth
+              sx={{mb: 2}}>
+                <InputLabel disableAnimation={false} >Estado del Libro</InputLabel>
+                <Select
+                  value={condition}
+                  onChange={handleConditionChange}
+                  required
+                >
+                  <MenuItem value="Nuevo">Nuevo</MenuItem>
+                  <MenuItem value="Casi Nuevo">Casi Nuevo</MenuItem>
+                  <MenuItem value="Usado">Usado</MenuItem>
+                </Select>
+              </FormControl>
+
+              <Autocomplete
+                fullWidth
+                id="grouped-demo"
+                options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                groupBy={(option) => option.firstLetter}
+                getOptionLabel={(option) => option.title}
+                sx={{
+                mb: 2 }}
+                renderInput={(params) => <TextField {...params} label="Libros Buscados" />}
+              />
+            </Grid>
           </Grid>
-        )}
-      </form>
-      <Button type="submit" variant="contained" color="primary">
-          Subir Publicación
-        </Button>
-    </Paper>
+
+          {/* Botón "Subir Publicación" a la derecha */}
+          <Grid container justifyContent="flex-end" sx={{ marginTop: '10px' }}>
+            <Button type="submit" variant="contained" color="primary">
+              Subir Publicación
+            </Button>
+          </Grid>
+        </form>
+      </Paper>
     </>
   );
 };
